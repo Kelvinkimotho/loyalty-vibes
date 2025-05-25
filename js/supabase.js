1082553364ab9bc17bd81fd14bc2c7deb2202b4c
Kelvinkimotho <kelvnkwaruingi@gmail.com>
2025-05-25 06:33:51 -0400

    update

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mtsdliquavtbwrkjtiou.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10c2RsaXF1YXZ0Yndya2p0aW91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjUxNTgsImV4cCI6MjA2Mzc0MTE1OH0.Pv3zzsE9x2eU4bRGmyWwpnPgI1UyJ3rWVKVnGjfrldM'
+const supabaseKey = 'vvvvv'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
