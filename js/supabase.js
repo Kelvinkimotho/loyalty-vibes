0240b07c3078a80c86ad37f56d9231727f54c246
Janeanny1 <janethmalikita@gmail.com>
2025-05-26 21:00:59 +0300

    updated

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 export const SUPABASE_CONFIG = {
   url: 'https://ubctxumyjegaudwfkydk.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViY3R4dW15amVnYXVkd2ZreWRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyMDc5MzIsImV4cCI6MjA2Mzc4MzkzMn0.TUasJ1_1ntbgxKrus9ogbVMkwJYNT0160K4FVV0MBXk'
+  key: 'wwww'
 };
 
 export const supabase = window.supabase.createClient(
